c0ffa64fac66d41e03c868a2ad544933a792a362
yaman-io <jz@yaman.io>
2020-05-21 17:50:14 +0900

    ..

--- src/adapter/mariadb/signIn.ts
+++ src/adapter/mariadb/signIn.ts
@@ -13,13 +13,21 @@ interface SignIn {
 }
 
 const signIn: SignIn = function (loginId, password) {
-    const userTable = process.env["USER_TABLE"];
-    const loginIdColumn = process.env["LOGIN_ID_COLUMN"];
-    const passwordColumn = process.env["PASSWORD_COLUMN"];
-    const userProfileIdColumn = process.env["USER_PROFILE_ID_COLUMN"];
+    const userTable = process.env["USER_TABLE"] || "users";
+    const loginIdColumn = process.env["LOGIN_ID_COLUMN"] || "loginId";
+    const passwordColumn = process.env["PASSWORD_COLUMN"] || "password";
+    const userProfileIdColumn = process.env["USER_PROFILE_ID_COLUMN"] || "profileId";
 
-    const selectUserQuery = `SELECT ${userProfileIdColumn} FROOM ${userTable} WHERE ${loginIdColumn} = '${loginId}' AND ${passwordColumn} = '${password}' `;
-    const insertHistoryQuery = `INSERT INTO ${} ${userProfileIdColumn} FROOM ${userTable} WHERE ${loginIdColumn} = '${loginId}' AND ${passwordColumn} = '${password}' `;
+    const historyTable = process.env["HISTORY_TABLE"] || "histories";
+    const historyLoginIdColumn = process.env["HISTORY_LOGIN_ID_COLUMN"] || "loginId";
+    // const historyOAuthTypeColumn = process.env["HISTORY_OAUTH_TYPE_COLUMN"] || 'oauthType';
+    const historySignInTimeColumn = process.env["HISTORY_SIGNIN_TIME_COLUMN"] || "signInTime";
+    const historyExpireTimeColumn = process.env["HISTORY_EXPIRE_TIME_COLUMN"] || "expireTime";
+
+    const tokenLifeTime = parseInt(process.env["TOKEN_LIFETIME"] || 300); 
+
+    const selectUserQuery = `SELECT ${userProfileIdColumn} FROM ${userTable} WHERE ${loginIdColumn} = '${loginId}' AND ${passwordColumn} = '${password}' `;
+    const insertHistoryQuery = `INSERT INTO ${historyTable} (${historyLoginIdColumn},${historySignInTimeColumn},${historyExpireTimeColumn}) VALUES ('${historyLoginIdColumn}',NOW(), NOW()) `;
 
     let connectionReserved: PoolConnection = null;
     let profileId: number;
